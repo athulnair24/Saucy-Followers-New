d0eaf74beb6f1fb3dd764c5a1e4217db287ef091
Justin Estrada <justin@taylordigital.com>
2018-09-29 22:41:25 -0700

    v0.1.1

--- saucy-followers.php
+++ saucy-followers.php
@@ -8,14 +8,14 @@
  * that starts the plugin.
  *
  * @link              https://mydigitalsauce.com/
- * @since             0.1.0
+ * @since             0.1.1
  * @package           Saucy_Followers
  *
  * @wordpress-plugin
  * Plugin Name:       Saucy Followers
  * Plugin URI:        https://mydigitalsauce.com/
  * Description:       Allows users to follow other users and see updates from users they follow.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            MyDigitalSauce
  * Author URI:        https://mydigitalsauce.com/
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 0.1.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SAUCE_FOLLOWERS_VERSION', '0.1.0' );
+define( 'SAUCE_FOLLOWERS_VERSION', '0.1.1' );
 
 /**
  * The code that runs during plugin activation.
